7d3d9996378cc37e4ced8480747d52ccf16c5f0b
Barak Daniel <71881281+barakk123@users.noreply.github.com>
2024-03-30 15:42:38 +0300

    Update dashboardIndex.tsx

--- ticket/src/pages/dashboardIndex.tsx
+++ ticket/src/pages/dashboardIndex.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import { Divider } from "@mui/material";
@@ -28,27 +27,13 @@ const boards = [
 export default function DashboardIndex() {
   const [boardsData, setBoardsData] = useState(boards);
 
-
-import React from "react";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import { Tooltip, TooltipProps, tooltipClasses } from "@mui/material";
-import styled from "@emotion/styled";
-
-export default function DashboardIndex() {
-
   return (
     <div className="dashboard-viewer">
       <h3 className="text-[32px] leading-10 font-semibold tracking-[-0.01em] flex items-center gap-4">
         Boards List
-
         <StyledTooltip arrow title="Complete view of all of your boards.">
           <InfoOutlinedIcon sx={{ width: 20, mt: "4px" }} />
         </StyledTooltip>
-
-        <BootstrapTooltip arrow title="Complete view of all of your boards.">
-          <InfoOutlinedIcon sx={{ width: 20, mt: "4px" }} />
-        </BootstrapTooltip>
-
       </h3>
       <h6 className="text-[17px] font-medium text-gray-500 tracking-[-0.01em]">
         Welcome back, User Name |{" "}
@@ -57,7 +42,6 @@ export default function DashboardIndex() {
           day: "numeric",
         })}
       </h6>
-
 
       <Divider sx={{ m: "20px 0" }} />
 
@@ -163,20 +147,3 @@ export default function DashboardIndex() {
     </div>
   );
 }
-    </div>
-  );
-}
-
-const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip {...props} arrow classes={{ popper: className }} />
-))(({ theme }) => ({
-  [`& .${tooltipClasses.arrow}`]: {
-    color: "black",
-  },
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: "black",
-    fontSize: 14,
-    padding: "5px 10px",
-    borderRadius: 8,
-  },
-}));
