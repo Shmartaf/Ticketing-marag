82394916fa8a7b5d484e2061c4cec59e4cd5288a
galavital13 <gal13031993@gmail.com>
2024-03-30 15:51:47 +0300

    LOG

--- ticket/src/components/Dashboard/DashboardLayout.tsx
+++ ticket/src/components/Dashboard/DashboardLayout.tsx
@@ -5,11 +5,7 @@ import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
   return (
-
     <div className="flex gap-5 w-screen">
-
-    <div className="grid grid-cols-[280px_auto] gap-7">
-
       <Sidebar />
       <Outlet /> {/* This is where nested routes will be rendered */}
     </div>
