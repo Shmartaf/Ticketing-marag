1694f88690b658cca52d21735ea4469ee1d7ab3c
Or Basker <or.basker@ridewithvia.com>
2024-04-09 19:43:30 +0300

    small update to creds supabase

--- ticket/src/lib/supabaseClient.jsx
+++ ticket/src/lib/supabaseClient.jsx
@@ -2,10 +2,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const createSupabaseClient = () => {
-  const url = import.meta.env.VITE_REACT_APP_SUPABASE_PROJECT_URL;
-  const key = import.meta.env.VITE_REACT_APP_SUPABASE_PROJECT_KEY;
-
-  const supabase = createClient(url, key);
-  // console.log('Supabase:', supabase);
-  return supabase;
+    const url = import.meta.env.VITE_REACT_APP_SUPABASE_PROJECT_URL;
+    const key = import.meta.env.VITE_REACT_APP_SUPABASE_PROJECT_KEY;
+    console.log('URL:', url);
+    console.log('Key:', key);
+    const supabase = createClient(url, key);
+    // console.log('Supabase:', supabase);
+    return supabase;
 };
